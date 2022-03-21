cf6afec55e276fedad89ae87c3e568c067184996
Lukasz Stasiak <lukasz.stasiak95@gmail.com>
2022-03-21 02:47:06 +0100

    Add Ml algorithm to registry

--- backend/apps/ml/income_classifier/random_forest.py
+++ backend/apps/ml/income_classifier/random_forest.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import joblib
 import pandas as pd
 
 
 class RandomForestClassifier:
     def __init__(self):
-        path_to_artifacts = "../../research/"
+        path_to_artifacts = "./research/"
         self.values_fill_missing =  joblib.load(path_to_artifacts + "train_mode.joblib")
         self.encoders = joblib.load(path_to_artifacts + "encoders.joblib")
         self.model = joblib.load(path_to_artifacts + "random_forest.joblib")
